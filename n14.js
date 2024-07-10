2d9856d687afc4b1a1cc33f4be70a6764baf9cfc
jeonheeae <gmldo2468@gmail.com>
2024-07-10 12:10:25 +0900

    n14수정중

--- n14.js
+++ n14.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("안녕하세요! Express로 만든 서버입니다.");
 });
-app.get(".list", (req, res) => {
+app.get("/list", (req, res) => {
   fs.readdir(__dirname, "utf-8", (err, data) => {
     res.send(data);
   });
