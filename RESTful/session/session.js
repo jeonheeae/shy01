52ae8f47078f3175f766493043044c7e576d070f
jeonheeae <gmldo2468@gmail.com>
2024-07-17 10:55:09 +0900

    session.js

--- RESTful/session/session.js
+++ RESTful/session/session.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const app = express(`body-parser`);
+const app = express();
 const port = 3000;
 
 app.listen(port, () => {
