0a4530b1876f5f19bf2489a236fe22e4b2f96f6a
jeonheeae <gmldo2468@gmail.com>
2024-07-31 16:05:16 +0900

    Delete 1.html/side directory

--- 1.html/side/env설정Test.js
+++
@@ -1,5 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config(); //환경실행
-
-const test = process.env.TEST;
-console.log(test);
--- 1.html/side/request_환율API.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const url =
-  "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=A15zesNdILoAWwVIBfSHtbeX2jOrSO7L&data=AP01";
-
-app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port}`);
-});
