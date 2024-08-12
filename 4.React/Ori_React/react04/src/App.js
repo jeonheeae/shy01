2634fac4a24d00d2851b7a4267870b87be59ddd8
jeonheeae <gmldo2468@gmail.com>
2024-08-12 14:12:01 +0900

    Ori_React

--- 4.React/Ori_React/react04/src/App.js
+++ 4.React/Ori_React/react04/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Cryp from "./pages/crypto";
+import Lstorage from "./pages/localstorage";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="/">Home</Link> | <Link to="/crypto">Crypto</Link> |{" "}
+      <Link to="/localstorage">Local storage</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/crypto" element={<Cryp />} />
+        <Route path="/localstorage" element={<Lstorage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
